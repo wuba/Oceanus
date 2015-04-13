da334437486f8a3c9cb31302f2b2a0b88c5ee640
Chen Yang <biocyy@gmail.com>
2015-04-13 16:51:26 +0800

    1. 修改批处理时缓存statement逻辑 2. 修正mybatis demo的配置错误

--- oceanus-all/oceanus-core/src/main/java/com/bj58/oceanus/core/context/TransactionContext.java
+++ oceanus-all/oceanus-core/src/main/java/com/bj58/oceanus/core/context/TransactionContext.java
@@ -39,73 +39,71 @@ import com.google.common.collect.Maps;
  * @author Service Platform Architecture Team (spat@58.com)
  */
 public class TransactionContext {
-	
-	static final ThreadLocal<TransactionContext> threadLocal = new ThreadLocal<TransactionContext>();
-	
-	/**
-	 * 在一次事务中，一个数据源对应一个connection
-	 */
-	final Map<String, Connection> connsInTransaction;
-	
-	/**
-	 * 在一次事务中,一个connection中的相同table对应同一个statement
-	 */
-	final Map<String, Statement> stmtsInTransaction;
-	
-	/**
-	 * 在一次事务中的sql
-	 */
-	final List<String> sqlsInTransaction;
-	
-	
-	public TransactionContext() {
-		connsInTransaction = Maps.newLinkedHashMap();
-		stmtsInTransaction = Maps.newLinkedHashMap();
-		sqlsInTransaction = Lists.newLinkedList();
-	}
-	
-	public static TransactionContext getContext() {
-		if(threadLocal.get() == null)
-			threadLocal.set(new TransactionContext());
-		
-		return threadLocal.get();
-	}
-	
-	public void setContext(TransactionContext context) {
-		threadLocal.set(context);
-	}
-	
-	public Connection getTransactionConnection(DataNode dataNode, 
-			Transporter<Boolean> isNewConn) throws SQLException {
-		
-		Connection connection = connsInTransaction.get(dataNode.getId());
-		if(connection == null){
-			connection = dataNode.getConnection();
-			connsInTransaction.put(dataNode.getId(), connection);
-			isNewConn.setValue(true);
-		}
-		
-		return connection;
-	}
-	
-	public Statement getTransactionStatement(Connection connection, 
-			RouteTarget target) {
-		return stmtsInTransaction.get(connection.toString() + target.getNameNode().toString());
-	}
 
-	public void setTransactionStatement(Connection connection, 
-			RouteTarget target, Statement statement) {
-		
-		stmtsInTransaction.put(connection.toString() + target.getNameNode().toString(), statement);
-		sqlsInTransaction.add(target.getExecuteInfo().getExecuteSql());
-	}
-	
-	public void release() {
-		for(Statement statement : stmtsInTransaction.values()) {
-			JdbcUtil.closeStatement(statement);
-		}
-		setContext(null);
-		sqlsInTransaction.clear();
-	}
+    static final ThreadLocal<TransactionContext> threadLocal = new ThreadLocal<TransactionContext>();
+
+    /**
+     * 在一次事务中，一个数据源对应一个connection
+     */
+    final Map<String, Connection>                connsInTransaction;
+
+    /**
+     * 在一次事务中,一个connection中的相同table对应同一个statement
+     */
+    final Map<String, Statement>                 stmtsInTransaction;
+
+    /**
+     * 在一次事务中的sql
+     */
+    final List<String>                           sqlsInTransaction;
+
+    public TransactionContext(){
+        connsInTransaction = Maps.newLinkedHashMap();
+        stmtsInTransaction = Maps.newLinkedHashMap();
+        sqlsInTransaction = Lists.newLinkedList();
+    }
+
+    public static TransactionContext getContext() {
+        if (threadLocal.get() == null) threadLocal.set(new TransactionContext());
+
+        return threadLocal.get();
+    }
+
+    public void setContext(TransactionContext context) {
+        threadLocal.set(context);
+    }
+
+    public Connection getTransactionConnection(DataNode dataNode, Transporter<Boolean> isNewConn) throws SQLException {
+
+        Connection connection = connsInTransaction.get(dataNode.getId());
+        if (connection == null) {
+            connection = dataNode.getConnection();
+            connsInTransaction.put(dataNode.getId(), connection);
+            isNewConn.setValue(true);
+        }
+
+        return connection;
+    }
+
+    public Statement getTransactionStatement(Connection connection, RouteTarget target) {
+
+        return stmtsInTransaction.get(connection.toString() + target.getNameNode().toString()
+                                      + target.getBatchItem().sql);
+    }
+
+    public void setTransactionStatement(Connection connection, RouteTarget target, Statement statement) {
+
+        stmtsInTransaction.put(connection.toString() + target.getNameNode().toString() + target.getBatchItem().sql,
+            statement);
+        sqlsInTransaction.add(target.getExecuteInfo().getExecuteSql());
+    }
+
+    public void release() {
+        for (Statement statement : stmtsInTransaction.values()) {
+            JdbcUtil.closeStatement(statement);
+        }
+        setContext(null);
+        sqlsInTransaction.clear();
+    }
 
 }
